acb8503e5c5aada52b167e0ecadf2fff4f72d6ad
XunOuyang <ouyangivan@gmail.com>
2019-05-12 20:23:21 -0400

    Update 236 lowest common ancestor for binary tree.py

--- tree/236 lowest common ancestor for binary tree.py
+++ tree/236 lowest common ancestor for binary tree.py
@@ -27,3 +27,31 @@ class Solution(object):
         else:
             return left or right
         return right
+
+ ``` //C++ version
+    /**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root == NULL || p == root || q == root)
+            return root;
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+        if(left != NULL && right != NULL)
+            return root;
+        else if(left == NULL)
+            return right;
+        else if(right == NULL)
+            return left;
+        return root;
+    }
+};
+```
