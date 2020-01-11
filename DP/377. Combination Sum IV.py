84be29875c16cab5612217697c76ed22f36d929b
XunOuyang <ouyangivan@gmail.com>
2020-01-11 18:15:48 -0500

    Create 377. Combination Sum IV.py

---
+++ DP/377. Combination Sum IV.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def combinationSum4(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        nums.sort()
+        dp = [1] + [0]*target
+        for i in range(target+1):
+            for num in nums:
+                if num > i:
+                    break
+                else:
+                    dp[i] += dp[i-num]
+        return dp[target]
