75d6a762db3c1d7a9dda28e5e7d8a4c6c5907504
XunOuyang <ouyangivan@gmail.com>
2020-01-10 22:17:36 -0500

    Update 47 permutation II.py

--- backtrack/47 permutation II.py
+++ backtrack/47 permutation II.py
@@ -1,8 +1,42 @@
-# -*- coding: utf-8 -*-
+"""
+Typical backtrack solution
+When we first get this solution, we need to sort them because duplicates exist.
+This problem is a little bit different then the others, in order to make sure that
+there will be no duplicates, we should make use visited to mark each elements that
+had been visited before. So does permutation I.
+"""
+
+class Solution(object):
+    def permuteUnique(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res = []
+        visited = [False]*len(nums)
+        nums.sort()
+        self.backtrack(nums, [], res, visited)
+        return res
+    
+    def backtrack(self, nums,  path, res, visited):
+        
+        if len(path) == len(nums):
+            res.append(path)
+        for i in range(len(nums)):
+            if not visited[i]:
+                if i>0 and nums[i] == nums[i-1] and not visited[i-1]:
+                    continue
+                visited[i] = True
+                self.backtrack(nums, path+[nums[i]], res, visited)
+                visited[i] = False
+
+
 """
 Created on Sun Jul 08 15:55:15 2018
 
 @author: TZLMYQ
+Solution 2:
+Actually this is not a backtrack method, it is a recursive method
 """
 
 class Solution(object):
