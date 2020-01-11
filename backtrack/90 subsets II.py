94e8f7671c6b7a6d34305df511402e180143ba62
XunOuyang <ouyangivan@gmail.com>
2020-01-11 12:48:14 -0500

    Create 90 subsets II.py

---
+++ backtrack/90 subsets II.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def subsetsWithDup(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res = []
+        nums.sort()
+        self.backtrack(nums, 0, [], res)
+        return res
+    
+    def backtrack(self, nums, index, path, res):
+        res.append(path)
+        for i in range(index, len(nums)):
+            if i > index and nums[i] == nums[i-1]:
+                continue
+            self.backtrack(nums, i+1, path+[nums[i]], res)
+        
