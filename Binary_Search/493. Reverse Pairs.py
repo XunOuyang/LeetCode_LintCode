639926da43d57028196a0731a3d7869b97dbe20e
XunOuyang <ouyangivan@gmail.com>
2020-02-05 22:26:49 -0500

    Create 493. Reverse Pairs.py

---
+++ Binary_Search/493. Reverse Pairs.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def reversePairs(self, nums):
+        stack = []
+        res = 0
+        while nums:
+            num = nums.pop()
+            index = bisect.bisect_left(stack, num/2.0)
+            res += index
+            bisect.insort(stack, num)
+        return res
