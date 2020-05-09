078ceab651837f3b6742b3ab2c7fe89e7021fc3d
XunOuyang <ouyangivan@gmail.com>
2020-05-09 11:25:58 -0400

    Update 992. Subarrays with K Different Integers.py

--- Two Pointer/992. Subarrays with K Different Integers.py
+++ Two Pointer/992. Subarrays with K Different Integers.py
@@ -1,7 +1,7 @@
 """
 这道题有三个要点。
 1.注意，subarray允许有重复的出现。这一点很关键，不然会是指数级别难度的增加。
-2. Exactly K distinct integer or element, 这个时候很容易自然想到sliding window。其实不用题中的两个相减的那种方法依然可以做。
+2. Exactly K distinct integer or element, 这个时候很容易自然想到sliding window。其实不用题中的两个相减的那种方法依然可以做。写起来会非常丑陋。
 3. 这道题的helper function里面并不要求左指针最后走到尽头，想一想这是为什么。
 """
 
