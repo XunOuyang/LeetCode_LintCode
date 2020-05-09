b62267b4e1ff734ed6978e130eb189bcc0f3c928
XunOuyang <ouyangivan@gmail.com>
2020-05-09 14:52:47 -0400

    Update and rename 48 matrix rotate.py to Array/48 matrix rotate.py

--- Array/48 matrix rotate.py
+++ Array/48 matrix rotate.py
@@ -17,6 +17,19 @@ class Solution(object):
             for j in range(i):
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
                 
+class Solution2:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        for i in range(len(matrix)):
+            for j in range(i + 1, len(matrix)):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        for i in range(len(matrix)):
+            for j in range(len(matrix)//2):
+                matrix[i][j], matrix[i][len(matrix)-1-j] = matrix[i][len(matrix)-1-j], matrix[i][j]
+        return matrix
+                
                 
 matrix = [
   [1,2,3],
