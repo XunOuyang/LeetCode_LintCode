f38d9bacb9c6f3a872bbb2f2ed17a3c2c841a082
XunOuyang <ouyangivan@gmail.com>
2020-05-22 22:54:32 -0400

    Create 824. Goat Latin.py

---
+++ string/824. Goat Latin.py
@@ -0,0 +1,10 @@
+class Solution:
+    def  toGoatLatin(self, S):
+        # 
+        words = S.split(" ")
+        for i in range(len(words)):
+            if words[i][0] not in "aeiouAEIOU":
+                words[i] = words[i][1:] + words[i][0]
+            words[i] += "ma" + "a" * (i+1)
+        return " ".join(words)
+                
