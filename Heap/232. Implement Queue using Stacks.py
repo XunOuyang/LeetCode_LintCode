7c3c9af769539c2f730400473ad8460f7f17464d
XunOuyang <ouyangivan@gmail.com>
2020-07-19 23:10:37 -0400

    Create 232. Implement Queue using Stacks.py

---
+++ Heap/232. Implement Queue using Stacks.py
@@ -0,0 +1,37 @@
+class MyQueue:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.stack = []
+        
+
+    def push(self, x: int) -> None:
+        """
+        Push element x to the back of queue.
+        """
+        temp = []
+        while self.stack:
+            temp.append(self.stack.pop())
+        temp.append(x)
+        while temp:
+            self.stack.append(temp.pop())
+
+    def pop(self) -> int:
+        """
+        Removes the element from in front of queue and returns that element.
+        """
+        return self.stack.pop()
+
+    def peek(self) -> int:
+        """
+        Get the front element.
+        """
+        return self.stack[-1]
+
+    def empty(self) -> bool:
+        """
+        Returns whether the queue is empty.
+        """
+        return self.stack == []
