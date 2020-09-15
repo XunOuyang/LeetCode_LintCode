e4c1fb323a5b9ad7c6222ed78055136bc3730f1e
XunOuyang <ouyangivan@gmail.com>
2020-09-15 19:37:59 -0400

    Update 198.House Robber.cpp

--- DP/198.House Robber.cpp
+++ DP/198.House Robber.cpp
@@ -3,16 +3,15 @@ public:
     int rob(vector<int>& nums) {
         if(nums.size() == 0)
             return 0;
-        int pre_0 = 0, pre_1 = nums[0];
-        int res = nums[0];
-        for(int i=1; i < nums.size(); i++)
+        else if(nums.size() == 1)
+            return nums[0];
+        int pre = 0, cur = 0, res = 0;
+        for(int i=0; i<nums.size(); i++)
         {
-            int temp_0 = max(pre_0, pre_1);
-            int temp_1 = max(pre_0 + nums[i], pre_1);
-            res = max(temp_0, temp_1);
-            pre_0 = temp_0;
-            pre_1 = temp_1;        
+            int temp = max(cur, pre + nums[i]);
+            pre = cur;
+            cur = temp;
         }
-        return res;
+        return cur;
     }
 };
