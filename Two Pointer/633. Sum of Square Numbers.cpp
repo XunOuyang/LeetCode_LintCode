a421442fafe1ff78cc7c97bb35ed20cf4594fd06
XunOuyang <ouyangivan@gmail.com>
2021-01-08 19:45:04 -0500

    Create 633. Sum of Square Numbers.cpp

---
+++ Two Pointer/633. Sum of Square Numbers.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        int left = 0, right = int(pow(c, 0.5));
+        while(left <= right)
+        {
+            long s = long(left * left) + long(right * right);
+            if(s == c)
+                return true;
+            else if(s < c)
+                left++;
+            else
+                right--;
+        }
+        return false;
+    }
+};
