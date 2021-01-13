46a89eddf44ad96b704b52acde428fa3e8a05c96
XunOuyang <ouyangivan@gmail.com>
2021-01-12 23:15:13 -0500

    Update and rename 1835. 停在原地的方案数1.cpp to 1269. Number of Ways to Stay in the Same Place After Some Steps.cpp

--- DP/1269. Number of Ways to Stay in the Same Place After Some Steps.cpp
+++ DP/1269. Number of Ways to Stay in the Same Place After Some Steps.cpp
@@ -1,31 +1,32 @@
 class Solution {
 public:
-    /**
-     * @param steps: steps you can move
-     * @param arrLen: the length of the array
-     * @return: Number of Ways to Stay in the Same Place After Some Steps
-     */
     int numWays(int steps, int arrLen) {
-        // write your code here
         if(arrLen == 0)
             return 1;
-        vector<int> pre(arrLen, 0);
-        vector<int> cur(arrLen, 0);
+        arrLen = min(arrLen, int(steps / 2) + 1);       // 一定要注意这一步的优化
+        vector<long> pre(arrLen, 0);
+        vector<long> cur(arrLen, 0);
         pre[0] = 1;
+        int const mod = int(pow(10, 9) + 7);
         for(int i = 1; i < steps + 1; i++)
         {
             cur[0] = pre[0] + pre[1];
+            cur[0] = cur[0] % mod;
             if(arrLen > 1)
             {
                 for(int j=1; j < arrLen - 1; j++)
                 {
                     cur[j] = pre[j-1] + pre[j] + pre[j+1]; 
+                    cur[j] %= mod;
                 }
             }
             if(arrLen)
+            {
                 cur[arrLen - 1] = pre[arrLen-1] + pre[arrLen - 2]; 
+                cur[arrLen - 1] %= mod;
+            }
             pre = cur;
         }
-        return cur[0] % int(pow(10, 9) + 7);
+        return int(cur[0]);
     }
 };
